7b48c84c7e8c59bc84e32323d01e2e5097ff9ac9
Luděk Roleček <ludek@rolecek.cz>
2024-10-20 19:23:46 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,12 @@
 import {useEffect, useState} from 'react'
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  "URL ADRESA SUPABASE PROJEKTU",
-  "ANON KLIC Z PROJEKTU"
-);
 
 function App() {
-
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const loadData = async () => {
-      const { data, error } = await supabase
-        .from("products")
-        .select()
-        .order('price')
-      setProducts(data);
-    }
-    loadData();
-  }, [])
-
-
-  const handleAddProduct = async () => {
-    const { data, error } = await supabase
-      .from('products')
-      .insert({
-        name: 'Susicka 10',
-        description: 'Huci, rotuje a susi',
-        price: 25000
-      })
-      .select()
-      .single()
-    console.log(data)
-  }
-
-  const handleChangeProduct = async () => {
-    const { data, error } = await supabase
-      .from('products')
-      .update({
-        name: 'Susicka 5000',
-        price: 5000
-      })
-      .eq('id', 5)
-      .select()
-      .single()
-
-    console.log(data)
-  }
-
-  const handleDeleteProduct = async () => {
-    const { data, error } = await supabase
-      .from('products')
-      .delete()
-      .eq('id', 6)
-
-    console.log(data)
-  }
 
 
   return (
     <>
       <h1>Supabase</h1>
 
-      <button onClick={handleAddProduct}>Pridat novy produkt</button>
-
-      <button onClick={handleChangeProduct}>Zmenit produkt</button>
-
-      <button onClick={handleDeleteProduct}>Smazat produkt</button>
-
-
-      <ul>
-        {products.map(product => <li key={product.id}>
-          <h2>{product.name}</h2>
-          <p>{product.description}</p>
-          <p><strong>{product.price}</strong></p>
-        </li>)}
-      </ul>
     </>
   )
 }
